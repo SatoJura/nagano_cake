04994d371810bf2a64b665494520b5a09973b943
SatoJura <jnnsk0snb@gmail.com>
2021-08-24 08:38:39 +0000

    [Add]管理者/商品新規登録

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -3,18 +3,27 @@ class Admin::ItemsController < ApplicationController
   end
   
   def new
+    @item = Item.new
   end
   
   def create
+    item = Item.new(item_params)
+    item.save
   end
   
   def show
+    @item = Item.find(params[:id])
   end
 
   def edit
   end
 
   def update
+  end
+  
+  private
+  def item_params
+    params.require(:item).permit(:name,:image,:introduction,:price,:is_active)
   end
 
 end

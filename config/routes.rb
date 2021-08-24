de5493441444f24d94074b107f3c12a5db35bb9a
SatoJura <jnnsk0snb@gmail.com>
2021-08-24 04:02:00 +0000

    [Add]会員/devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
+  # 管理者側
   devise_for :admins, controllers: {
-   sessions: "admins/sessions"
+   ssessions:      'admins/sessions',
+   passwords:      'admins/passwords'
  }
-  
+
+  # 会員側
+  devise_for :customers, controllers: {
+    sessions:      'customers/sessions',
+    passwords:     'customers/passwords',
+    registrations: 'customers/registrations'
+  }
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

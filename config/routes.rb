dd3c65d9c3ddbb9de39094ba6b1abdc06a2e5d52
SatoJura <jnnsk0snb@gmail.com>
2021-08-24 05:04:12 +0000

    [Add]管理者/ジャンルのビュー・コントローラ作成

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,19 @@ Rails.application.routes.draw do
    passwords:      'admins/passwords'
  }
 
+ namespace :admin do
+     resources :genres, only: [:index, :create, :update, :edit]
+ end
+
   # 会員側
   devise_for :customers, controllers: {
     sessions:      'customers/sessions',
     passwords:     'customers/passwords',
     registrations: 'customers/registrations'
   }
+
+  scope module: :customer do
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

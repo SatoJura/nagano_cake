96b02e025686e5927ee0db22c429d14be86dc321
SatoJura <jnnsk0snb@gmail.com>
2021-08-25 14:43:16 +0000

    [Add]管理者/会員一覧

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,16 +1,17 @@
 class Admin::ItemsController < ApplicationController
   def index
   end
-  
+
   def new
     @item = Item.new
   end
-  
+
   def create
     item = Item.new(item_params)
     item.save
+    redirect_to admin_item_path(item.id)
   end
-  
+
   def show
     @item = Item.find(params[:id])
   end
@@ -20,10 +21,10 @@ class Admin::ItemsController < ApplicationController
 
   def update
   end
-  
+
   private
   def item_params
-    params.require(:item).permit(:name,:image,:introduction,:price,:is_active)
+    params.require(:item).permit(:genre_id,:name,:image,:introduction,:price,:is_active)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'customers/index'
+    get 'customers/show'
+    get 'customers/edit'
+    get 'customers/update'
+  end
   # 管理者側
   devise_for :admins, controllers: {
    ssessions:      'admins/sessions',

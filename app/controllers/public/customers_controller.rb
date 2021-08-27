3aac03b66cd05949f3bd12e8d0ffb655ba406f85
SatoJura <jnnsk0snb@gmail.com>
2021-08-27 06:31:05 +0000

    [Add]会員/会員マイページレイアウト

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -18,4 +18,9 @@ class Public::CustomersController < ApplicationController
   def change
 
   end
+  
+  private
+  def customer_params
+    params.require(:customer).permit(:last_name,:first_name,:last_name_kana,:first_name_kana,:postal_code,:address,:telephone_number,:is_active)
+  end
 end

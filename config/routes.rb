cde9a571ecc001fc65bd4aa6fd47e2f13a182e8a
SatoJura <jnnsk0snb@gmail.com>
2021-08-29 14:23:33 +0000

    [Add]会員/カート商品コントローラ・ビュー作成

--- config/routes.rb
+++ config/routes.rb
@@ -15,22 +15,25 @@ Rails.application.routes.draw do
 
   # 会員側
   devise_for :customers
-  
+
   scope module: :public do
     resource :customers, only: [:edit, :update]
     get "customers/my_page" => "customers#show"
     get "customers/unsubscribe" => "customers#unsubscribe"
     patch "customers/change" => "customers#change"
-    
+
     root to: 'homes#top'
     get 'about' => "homes#about"
-    
+
     post 'orders/confirm'
     get 'orders/thanks'
     resources :orders, only: [:new, :create, :index, :show]
-    
+
     resources :items, only: [:index, :show]
-    
+
+    delete 'cart_items/destroy_all'
+    resources :cart_item, only: [:index, :create, :update, :destroy]
+
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

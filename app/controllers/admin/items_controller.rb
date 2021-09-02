4353d75e7b90876a9931aefdb615ac7162dcd888
SatoJura <jnnsk0snb@gmail.com>
2021-09-02 06:15:48 +0000

    [Add]管理者/部分テンプレ作成

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -18,9 +18,14 @@ class Admin::ItemsController < ApplicationController
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
 
   def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to admin_item_path(@item.id)
+    end
   end
 
   private

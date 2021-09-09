7ebb4474cf8a5709e55aea65230b3a581100f044
SatoJura <jnnsk0snb@gmail.com>
2021-09-09 08:46:32 +0000

    [Add]publicsコントローラ作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,28 +2,26 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
-    if current_customer
-      flash[:notice] = "会員ログインに成功しました"
-      root_path
-    elsif @admin
+    if current_admin
       flash[:notice] = "管理者ログインに成功しました"
       admin_path
     else
-      flash[:notice] = "会員新規登録が完了しました"
-      customers_my_page_path
+      flash[:notice] = "会員ログインに成功しました"
+      root_path
     end
   end
 
-# 各sign_out後の遷移先が指定できない
+      # flash[:notice] = "会員登録に成功しました"
+      # customers_my_page_path
+
+
   def after_sign_out_path_for(resource)
-    if current_customer
+    if resource == :customer
       flash[:notice] = "会員ログアウトに成功しました"
       root_path
-    elsif @admin
+    else
       flash[:notice] = "管理者ログアウトに成功しました"
       new_admin_session_path
-    else
-      root_path
     end
   end
 

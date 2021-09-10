b761b9d56d011241357e95d50c8173187e48dadd
SatoJura <jnnsk0snb@gmail.com>
2021-09-10 07:13:00 +0000

    [Add]会員/登録情報編集画面作成

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -3,11 +3,17 @@ class Public::CustomersController < ApplicationController
   end
 
   def edit
-
+    @customer = current_customer
   end
 
   def update
-
+    @customer = current_customer
+    if @customer.update(customer_params)
+      flash[:notice]="編集に成功しました"
+      redirect_to customers_my_page_path
+    else
+      render "edit"
+    end
   end
 
   def unsubscribe
@@ -20,6 +26,6 @@ class Public::CustomersController < ApplicationController
 
   private
   def customer_params
-    params.require(:customer).permit(:last_name,:first_name,:last_name_kana,:first_name_kana,:postal_code,:address,:telephone_number,:is_active,:email)
+    params.require(:customer).permit(:last_name,:first_name,:last_name_kana,:first_name_kana,:postal_code,:address,:telephone_number,:email)
   end
 end

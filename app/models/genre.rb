bc05e95a164a971d61017ab952560416d023a8e9
SatoJura <jnnsk0snb@gmail.com>
2021-09-18 10:42:29 +0000

    [Add]ヘッダー検索窓　形のみ

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -2,5 +2,13 @@ class Genre < ApplicationRecord
   has_many :items
 
   validates :name, presence: true
+  
+  def self.serch(serch)
+    if serch
+      Genre.where(['name LIKE ?', "%#{serch}%"])
+    else
+      Genre.all
+    end
+  end
 
 end
